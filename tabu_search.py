298a613e2fa11c8ab5460191871b0795e5dbf2e7
Damian Cholewa <cholewa.damian1997@gmail.com>
2019-12-01 18:44:20 +0100

    Update tabu_search.py

--- tabu_search.py
+++ tabu_search.py
@@ -1,51 +1,127 @@
 import numpy as np
 
-"""
-x-  6 elementowy wektor  stanu reprezentujący poziomy poszczególnych budynków
-x0 - poziom ratusza
-x1 - poziom tartaku
-x2 - poziom ciegielni
-x3 - poziom huty żelaza
-x4 - poziom zagrody
-x5 - poziom spichlerza
-"""
-x = np.array([0, 0, 0, 0, 0, 0])
+import data
 
-"""
-x0_demand  - macierz określa zapotrzebowanie  do budowy kolejnych poziomów ratusza
-stopien | drewno | cegła | żelazo | pracownicy
-   1    |        |       |        | 
-   2    |        |       |        |  
-  ...   |        |       |        |  
-   25   |        |       |        |                
-"""
 
-x0_demand = np.array([[90, 80, 70, 5],
-                     [113, 102, 88, 6, 91],
-                     [143, 130, 111, 7, 86],
-                     [180, 166, 140, 8, 82],
-                     [227, 211, 176, 9, 78],
-                     [286, 270, 222, 11, 75],
-                     [360, 344, 280, 13, 71],
-                     [454, 438, 353, 15, 68],
-                     [572, 559, 445, 18, 64],
-                     [720, 712, 560, 21, 61],
-                     [908, 908, 706, 24, 58],
-                     [1144, 1158, 890, 28, 56],
-                     [1441, 1476, 1121, 33, 53],
-                     [1816, 1882, 1412, 38, 51],
-                     [2288, 2400, 1779, 45, 48],
-                     [2883, 3060, 2242, 53, 46],
-                     [3632, 3902, 2825, 62, 44],
-                     [4577, 4975, 3560, 72, 42],
-                     [5767, 6343, 4485, 84, 40],
-                     [7266, 8087, 5651, 99, 38],
-                     [9155, 10311, 7120, 116, 36],
-                     [11535, 13146, 8972, 135, 34],
-                     [14534, 16762, 11304, 158, 33],
-                     [18313, 21371, 14244, 185, 31],
-                     [23075, 27248, 17947, 216, 30]])
+# tmp
+def do_reset():
+    pass
 
+
+# tmp
+def time(solution):
+    pass
+
+
+class RecentlyUsedSolution:
+    """Class used for storing previous solution
+
+    attributes
+    -------
+    _recent_solution : list
+        list of recent solution
+
+    """
+    def __init__(self):
+        self._recent_solution = list()
+
+    def check_if_used(self, solution, generations=3):
+        """Method used for checking if given solution was found before
+
+        Parameters
+        ----------
+        solution : list
+            list representing solution
+
+        generations: int
+            number of how many previous solutions will be consider
+
+        Returns
+        -------
+        bool
+            True if solution was found before, False otherwise
+        """
+
+        if len(self._recent_solution) < generations:
+            pass    # Error codes ???
+
+        used = self._recent_solution[-generations:]
+        if solution in used:
+            return True
+        else:
+            return False
+
+    def add(self, solution):
+        """ Appending solution to list
+
+        Parameters
+        ---------
+        solution: list
+            list representing solution
+        """
+        self._recent_solution.append(solution)
+
+    def check_progress(self, time_const, generations):
+        """ Checking if solutions were decreasing in given generations more than given time_const
+        Parameters
+        ----------
+        time_const: float
+            value of time needed to be consider if solutions are decreasing
+
+        generations: int
+            number of how many previous solution will be consider
+
+        Returns
+        -------
+        bool
+            True if progress exist, False otherwise
+        """
+
+        if len(self._recent_solution) < generations:
+            pass    # Error codes ???
+        used = self._recent_solution[-generations:]
+
+        progress = True
+        for i in range(len(used)-1):
+            if used[i] - time_const < used[i+1]:
+                progress = False
+            else:
+                continue
+
+        # ToDO: other rules to check progress ?
+        return progress
+
+    def check_diversity(self, generations):
+        """
+        Check if diversity is present in search process
+
+        Parameters
+         ---------
+        generations: int
+            number of how many previous solutions will be consider
+
+        Return:
+        --------
+        bool
+            True if diversity is present in search process, False otherwise
+        """
+        if len(self._recent_solution) < generations:
+            pass    # Error codes ???
+        used = self._recent_solution[-generations:]
+
+        diversity = True
+        changes_list = list()
+        for i in range(len(used[0])): 
+            changes_counter = 0
+            for j in range(len(used)-1):
+                if used[j][i] != used[j+1][i]:
+                    changes_counter += 1
+            
+            changes_list.append(changes_counter)
+            
+        # ToDO: if statement for checking changes list and setting diversity variable
+        
+        return diversity
 
 
 
