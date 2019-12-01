660519cf97d357220bedc4538185697cf9ee8263
Jakub9999 <58400449+Jakub9999@users.noreply.github.com>
2019-12-01 23:13:06 +0100

    Update tabu_search.py

--- tabu_search.py
+++ tabu_search.py
@@ -1,23 +1,18 @@
-
 import numpy as np
 
-def generete_time_of_solution(solution): pass
+def generete_solution_time(solution): pass
 
 
 
 def generate_first_solution():
-    """generuje pierwsze rozwiązanie, taka forma żeby było że ten ratusz jest pierwszy zawsze """
 
     first_solution = np.array([0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5])
     return first_solution
 
 
 def create_new__rand_solutionsList(solution, amount_of_solutions):
-    "generuje liste rozwiązań w każdym jest jedna zmiana w stosunku do podanego wszystkie są od siebie różne "
 
     new_solution_List = np.array([])
-    changed_elem_List = np.array([])
-
     for x in range(0, amount_of_solutions):
         first = np.random.random_integers(np.len(solution))
         second = np.random.random_integers(np.len(solution))
@@ -26,30 +21,60 @@ def create_new__rand_solutionsList(solution, amount_of_solutions):
 
         new_solution = solution
         new_solution[first], new_solution[second] = new_solution[second], new_solution[first]
-        changed_elem = np.array([first, second])
-
         if np.isin(new_solution_List, new_solution):
-            new_solution_List.append(new_solution_tuple)
-        if np.isin(changed_elem_List, changed_elem):
-            changed_elem_List.append(changed_elem)
-    new_solution_tuple = (new_solution_List, changed_elem_List)
-    return new_solution_tuple
+            new_solution_List.append(new_solution)
+
+    return new_solution_List
 
 def tabu_search(first_solution, iterations, tabu_list_size):
 
 
     solution = first_solution
     tabu_list = np.array([])
-    best_time = generete_time_of_solution(solution)
+    best_time_ever = generete_solution_time(solution)
     best_solution_ever = solution
 
     while count <= iterations:
 
-        """tu kuba możesz wsadzić tą logike """
+        available_solutions, changing_pairs_list = create_new__rand_solutionsList()
+        best_solution_index = 0
+        best_solution = available_solutions[0]
+        best_solution_time = generete_solution_time(best_solution)
+        forbidden_solution_list = None
+
+        for i in range(1, len(available_solutions)): # zaczynamy od 1 bo rozwiazanie z indexem zerowym jest punktem wyjscia
+            current_solution = available_solutions[i]
+            if generete_solution_time(current_solution) < best_solution_time:
+                best_solution = current_solution
+                best_solution_time = generete_solution_time(current_solution)
+                best_solution_index = i
+
+        found = False
+        while found == False:
+
+            if changing_pairs_list[best_solution_index] not in tabu_list:
+                tabu_list.append(changing_pairs_list[best_solution_index])
+                found = True
+                if best_solution_time < best_time_ever:
+                    best_time_ever = best_solution_time
+                    best_solution_ever = best_solution
+
+            else:
+                forbidden_solution_list.append(best_solution)
+                for i in range(1, len(available_solutions)):
+                    current_solution = available_solutions[i]
+                    if generete_solution_time(current_solution) < best_solution_time and current_solution not in forbidden_solution_list:
+                        best_solution = current_solution
+                        best_solution_time = generete_solution_time(current_solution)
+                        best_solution_index = i
 
         if len(tabu_list) >= tabu_list_size:
             tabu_list.pop(0)
 
         count = count + 1
 
-    return best_solution_ever, best_time
+    return best_solution_ever, best_time_ever
+
+
+
+
