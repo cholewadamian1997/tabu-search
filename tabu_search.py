4aed3906b14109b187157268926efe665030ac11
BercikHubercik <58395369+BercikHubercik@users.noreply.github.com>
2020-01-05 12:39:17 +0100

    Update tabu_search.py

--- tabu_search.py
+++ tabu_search.py
@@ -1,15 +1,22 @@
-
 import random
+import copy
 
 def generete_time_of_solution(solution): pass
-
 
 
 def generate_first_solution():
     """generuje pierwsze rozwiązanie, taka forma żeby było że ten ratusz jest pierwszy zawsze """
 
-    first_solution = [0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5]
+    first_solution = [0, 1, 2, 3, 4, 5,0, 1, 2, 3, 4, 5]
     return first_solution
+
+def is_solution_acceptable(new_solution):
+    levelList = [0,0,0,0,0,0]
+    for x in new_solution:
+        levelList[x] += 1
+        if levelList[0] < levelList[1] or levelList[0] < levelList[2] or levelList[0] < levelList[3]or levelList[0] < levelList[4]or levelList[0] < levelList[5]:
+            return False
+    return True
 
 
 def create_new__rand_solutionsList(solution, amount_of_solutions):
@@ -25,21 +32,28 @@ def create_new__rand_solutionsList(solution, amount_of_solutions):
             first = random.randrange(len(solution))
 
         temp_solution = copy.copy(solution)
-        print(temp_solution)
         temp_solution[first], temp_solution[second] = temp_solution[second], temp_solution[first]
         changed_elem = [first, second]
-        new_solution = copy.copy(temp_solution)
-        "sprawdzam czy nowe rozw spełnia ograniczenia(ratusz)"
 
-        if new_solution not in new_solution_List:
-            new_solution_List.append(new_solution)
-        if changed_elem not in changed_elem_List:
-            changed_elem_List.append(changed_elem)
-        print(new_solution)
+        "sprawdzam czy nowe rozw spełnia ograniczenia(ratusz)"
+        if is_solution_acceptable(temp_solution):
+            new_solution = copy.copy(temp_solution)
+            if new_solution not in new_solution_List:
+                new_solution_List.append(new_solution)
+            if changed_elem not in changed_elem_List:
+                changed_elem_List.append(changed_elem)
+
+        else:
+            x -= 1
+
+
+
+
+    new_solution_tuple = (new_solution_List, changed_elem_List)
+    return new_solution_tuple
+
 
 def tabu_search(first_solution, iterations, tabu_list_size):
-
-
     solution = first_solution
     tabu_list = []
     best_time = generete_time_of_solution(solution)
@@ -55,3 +69,4 @@ def tabu_search(first_solution, iterations, tabu_list_size):
         count = count + 1
 
     return best_solution_ever, best_time
+
