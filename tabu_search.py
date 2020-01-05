70ffe1aa15058323eea3586c55bc553a9cd4db6a
BercikHubercik <58395369+BercikHubercik@users.noreply.github.com>
2020-01-05 10:41:03 +0100

    Update tabu_search.py

--- tabu_search.py
+++ tabu_search.py
@@ -1,5 +1,5 @@
 
-import numpy as np
+import random
 
 def generete_time_of_solution(solution): pass
 
@@ -8,29 +8,29 @@ def generete_time_of_solution(solution): pass
 def generate_first_solution():
     """generuje pierwsze rozwiązanie, taka forma żeby było że ten ratusz jest pierwszy zawsze """
 
-    first_solution = np.array([0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5])
+    first_solution = [0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5]
     return first_solution
 
 
 def create_new__rand_solutionsList(solution, amount_of_solutions):
     "generuje liste rozwiązań w każdym jest jedna zmiana w stosunku do podanego wszystkie są od siebie różne "
 
-    new_solution_List = np.array([])
-    changed_elem_List = np.array([])
+    new_solution_List = []
+    changed_elem_List = []
 
     for x in range(0, amount_of_solutions):
-        first = np.random.random_integers(np.len(solution))
-        second = np.random.random_integers(np.len(solution))
+        first = random.randrange(len(solution))
+        second = random.randrange(len(solution))
         while first == second:
-            first = np.random.random_integers(np.len(solution))
+            first = random.randrange(len(solution))
 
         new_solution = solution
         new_solution[first], new_solution[second] = new_solution[second], new_solution[first]
-        changed_elem = np.array([first, second])
+        changed_elem = [first, second]
 
-        if np.isin(new_solution_List, new_solution):
-            new_solution_List.append(new_solution_tuple)
-        if np.isin(changed_elem_List, changed_elem):
+        if new_solution not in new_solution_List:
+            new_solution_List.append(new_solution)
+        if changed_elem not in changed_elem_List:
             changed_elem_List.append(changed_elem)
     new_solution_tuple = (new_solution_List, changed_elem_List)
     return new_solution_tuple
@@ -39,7 +39,7 @@ def tabu_search(first_solution, iterations, tabu_list_size):
 
 
     solution = first_solution
-    tabu_list = np.array([])
+    tabu_list = []
     best_time = generete_time_of_solution(solution)
     best_solution_ever = solution
 
